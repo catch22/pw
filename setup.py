34d7fa4be34870bc5a0b6af0ac24cebcea761332
Michael Walter <michael@slaptop.local>
2011-02-20 17:08:42 +0100

    Grrr.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname, abspath
 
 setup(
   name = 'pw',
-  version = '0.1.1',
+  version = '0.1.2',
   description = 'Grep GPG-encrypted YAML password safe.',
   author = 'Michael Walter',
   author_email = 'michael.walter@gmail.com',
