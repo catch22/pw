ed97da782b896e80ba2eedbc77708d0057fa12a3
Michael Walter <michael.walter@gmail.com>
2015-09-01 00:47:31 -0700

    Fixed add_a_line.py some more

--- test/add_a_line.py
+++ test/add_a_line.py
@@ -1,4 +1,9 @@
-import sys
+import os, sys
 
+# this script is a faux PW_EDITOR, which simply appends the string argv[1] to
+# the file argv[2]. it is called from test_cli.py:test_with_changes().
 assert len(sys.argv) == 3
-open(sys.argv[2], 'w').write(sys.argv[1] + '\n')
+with open(sys.argv[2], 'a') as fp:
+    fp.write('\n%s\n' % sys.argv[1])
+    fp.flush()
+    os.fsync(fp.fileno())
